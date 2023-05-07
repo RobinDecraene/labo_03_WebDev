81f1e8512660fa5b3e45ac2200991c61d7e44e18
Decraene Robin <robin.decraene@student.arteveldehs.be>
2023-05-07 15:57:31 +0200

    commit

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,36 +7,36 @@
 @section('content')
 
   <form action="{{ route('store') }}" method="POST">
-    @csrf
-    @method('POST')
+        @csrf
+        @method('POST')
 
-    <h1>Voeg een nieuwe afbeelding toe</h1>
+        <h1>Voeg een nieuwe afbeelding toe</h1>
 
-    @if($errors->any())
-    <ul style="color: red;">
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+        @if($errors->any())
+        <ul style="color: red;">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
-<div>
-  <label for="bronlocatie" class="form-label">Bronlocatie</label>
-  <input
-      type="text"  name="bronlocatie" id="bronlocatie" value="{{ old('bronlocatie') }}"
-      @if($errors->has('bronlocatie')) style="border-color: red;" @endif>
-</div>
+    <div>
+      <label for="bronlocatie" class="form-label">Bronlocatie</label>
+      <input
+          type="text"  name="bronlocatie" id="bronlocatie" value="{{ old('bronlocatie') }}"
+          @if($errors->has('bronlocatie')) style="border-color: red;" @endif>
+    </div>
 
-<div>
-  <label for="image" class="form-label">Image</label>
-  <input
-      type="text"  name="image" id="image" value="{{ old('image') }}"
-      @if($errors->has('image')) style="border-color: red;" @endif>
-</div>
+    <div>
+      <label for="image" class="form-label">Image</label>
+      <input
+          type="text"  name="image" id="image" value="{{ old('image') }}"
+          @if($errors->has('image')) style="border-color: red;" @endif>
+    </div>
 
-<div>
-  <button type="submit" >Voeg nieuwe afbeelding toe</button>
-</div>
+    <div>
+      <button type="submit" >Voeg nieuwe afbeelding toe</button>
+    </div>
 
   </form>
 
